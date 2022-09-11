0cc20d4e8b30aabaf8b76632cdfd6771b609a188
Pradyumna1312 <54210084+Pradyumna1312@users.noreply.github.com>
2022-09-11 22:38:32 +0530

    Added Model Test Bench and Mux_1 TB

--- PS 1/Simulation Files/MuxTb.sv
+++ PS 1/Simulation Files/MuxTb.sv
@@ -1,10 +1,22 @@
 module MuxTb();
-reg en, A, B, Y;
-Mux2_1 m(A,B,en,Y);
-initial begin
-en=0; A=1; B=0;
-#10 en=1;
-#20 en=0;
+reg[1:0] en;
+reg A, B, C, D, Y;
+reg clk=0;
+Mux4_1 m(A,B,C,D,en,Y);
+
+initial begin 
+	A=1; B=0; C=1; D=1'bX;
+	forever #10 clk=~clk;
+	end
+// Creating a array for Values of A, B, C, D
+reg val[0:3] = {1'b1, 1'b0, 1'b1, 1'bX};
+always@(posedge clk) begin 
+ en = $random;
+end
+
+always@(negedge clk) begin
+$display("%0b %0b", A, B);
+assert(Y === val[en]);
 end
 	
 endmodule
