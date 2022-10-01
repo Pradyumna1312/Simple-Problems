41c8c46861d7cfaa108c8efda18807f1b58a1a99
Narsepalli Pradyumna <npradyu@gmail.com>
2022-10-01 18:18:06 +0530

    Synthsis using Yosys

--- PS_3/Design_Files/Sort3ip.v
+++ PS_3/Design_Files/Sort3ip.v
@@ -1,10 +1,12 @@
-module Sort3ip();
-input [4:0] A,B,C;
-output [4:0] G,M,L;
-wire [4:0] G1,L1,G2,L2;
+module Sort3ip(input [4:0] A,B,C,
+output [4:0] G,M,L);
+wire [4:0] G1,L1,G2,L2,G3,L3;
 Sort_2ip s1(A,B,G1,L1);
 Sort_2ip s2(G1,C,G2,L2);
-assign G = G1;
-assign M = G2;
-assign L = L2;
+Sort_2ip s3(L1,C,G3,L3);
+assign G = G2;
+assign M = L2;
+assign L = L3;
 endmodule
+
+
--- PS_3/Design_Files/Sort_2ip.v
+++ PS_3/Design_Files/Sort_2ip.v
@@ -13,3 +13,4 @@ L <= A;
 end
 end
 endmodule
+
